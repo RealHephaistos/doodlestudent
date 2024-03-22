eaf6c99713a1208c41e831095a7ee04988fa3c9c
Berrouche Issameddine <issameddine.berrouche@etudiant.univ-rennes1.fr>
2024-03-22 21:53:01 +0100

    add argument to quiclaunch

--- quicklaunch.sh
+++ quicklaunch.sh
@@ -1,9 +1,34 @@
-cd api
-sudo docker build -t mhib/tlcbackend .
-sudo docker run -d --name tlc-backend -p 80:80 mhib/tlcbackend
-cd ..
-cd front
-sudo docker build -t mhib/tlcfrontend .
-sudo docker run -d --name tlc-frontend -p 80:80 mhib/tlcfrontend
-cd ..
+#!/bin/bash
+
+run_backend() {
+    echo "Building and running backend service..."
+    cd api
+    sudo docker build -t mhib/tlcbackend .
+    sudo docker rm -f tlc-backend
+    sudo docker run -d --name tlc-backend -p 80:80 mhib/tlcbackend
+    cd ..
+}
+
+run_frontend() {
+    echo "Building and running frontend service..."
+    cd front
+    sudo docker build -t mhib/tlcfrontend .
+    sudo docker rm -f tlc-frontend
+    sudo docker run -d --name tlc-frontend -p 80:80 mhib/tlcfrontend
+    cd ..
+}
+
+if [ "$1" = "backend" ]; then
+    run_backend
+fi
+
+if [ "$1" = "front" ]; then
+    run_frontend
+fi
+
+if [ "$1" = "backend" ] && [ "$2" = "front" ]; then
+    run_backend
+    run_frontend
+fi
+
 sudo docker-compose up -d
