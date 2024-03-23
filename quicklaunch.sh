9a006ca84589c524209c46ca40c8a5431c2bf5b6
Berrouche Issameddine <issameddine.berrouche@etudiant.univ-rennes1.fr>
2024-03-23 11:31:30 +0100

    update quicklaunch bash

--- quicklaunch.sh
+++ quicklaunch.sh
@@ -1,52 +1,69 @@
 #!/bin/bash
-
-run_backend() {
-    echo "Building and running backend service..."
-    cd api
-    sudo docker build -t mhib/tlcbackend .
-    cd ..
-}
-
-run_frontend() {
-    echo "Building and running frontend service..."
-    cd front
-    sudo docker build -t mhib/tlcfrontend .
-    cd ..
-}
+# Script de lancement rapide pour le projet TLC
 
 clean() {
     echo "Cleaning up..."
+    # Retire les containers
     sudo docker-compose down
 
+    # Retire les images
     sudo docker rmi mhib/tlcfrontend -f
     sudo docker rmi mhib/tlcbackend -f
 
-    sudo sed -i '/# TLC/d' /etc/hosts
-    sudo sed -i '/mhib.myadmin.tlc.fr/d' /etc/hosts
-    sudo sed -i '/mhib.doodle.tlc.fr/d' /etc/hosts
-    sudo sed -i '/mhib.pad.tlc.fr/d' /etc/hosts
+    # Retire les ips du fichier hosts
     sudo sed -i '/api/d' /etc/hosts
     sudo sed -i '/myadmin/d' /etc/hosts
     sudo sed -i '/etherpad/d' /etc/hosts
 
+    # Retire les configurations nginx
     sudo rm /etc/nginx/sites-enabled/tlc
     sudo service nginx restart
+    echo "\n"
+}
+
+build_backend() {
+    echo "Building backend image and saving it as .tar..."
+    cd api
+    sudo docker build -t mhib/tlcbackend .
+    sudo docker save -o backend.tar mhib/tlcbackend
+    echo "\n"
+    cd ..
+}
+
+build_frontend() {
+    echo "Building backend image and saving it as .tar..."
+    cd front
+    sudo docker build -t mhib/tlcfrontend .
+    sudo docker save -o frontend.tar mhib/tlcfrontend
+    echo "\n"
+    cd ..
+}
+
+push_images() {
+    echo "Pushing images to Docker registry..."
+    sudo docker load -i api/backend.tar
+    sudo docker load -i front/frontend.tar
+    sudo docker push mhib/tlcbackend
+    sudo docker push mhib/tlcfrontend
+    sudo docker tag mhib/tlcbackend mhib/tlcbackend:latest
+    sudo docker tag mhib/tlcfrontend mhib/tlcfrontend:latest
+    echo "\n"
 }
 
 compose() {
     echo "Running docker-compose..."
     sudo docker-compose up -d
+    echo "\n"
 }
 
 config_dns() {
     echo "Configuring DNS..."
     sudo echo "# TLC" >> /etc/hosts
-    sudo echo "127.0.0.1 mhib.myadmin.tlc.fr" >> /etc/hosts
-    sudo echo "127.0.0.1 mhib.doodle.tlc.fr" >> /etc/hosts
-    sudo echo "127.0.0.1 mhib.pad.tlc.fr" >> /etc/hosts
     sudo echo "127.0.0.1 api" >> /etc/hosts
     sudo echo "127.0.0.1 myadmin" >> /etc/hosts
     sudo echo "127.0.0.1 etherpad" >> /etc/hosts
+    echo "\n"
+    #TODO: Add DNS configuration based on docker network IP
 }
 
 set_nginx_config(){
@@ -54,6 +71,7 @@ set_nginx_config(){
     sudo cp nginx.conf /etc/nginx/sites-available/tlc
     sudo ln -s /etc/nginx/sites-available/tlc /etc/nginx/sites-enabled/
     sudo service nginx restart
+    echo "\n"
 }
 
 show_help() {
@@ -61,12 +79,14 @@ show_help() {
     echo "Options:"
     echo "-h, --help: Display this help message"
     echo "-c, --clean: Clean up docker containers and images"
-    echo "-b, --backend: Build and run the backend service"
-    echo "-f, --front: Build and run the frontend service"
+    echo "-b, --backend: Build and save the backend image"
+    echo "-f, --front: Build and save the frontend image"
+    echo "-p, --push: Push images to Docker registry"
     echo "-d, --docker-compose: Run docker-compose"
-    echo "-C, --config-dns: Configure DNS"
+    echo "-C, --config-dns: Configure DNS IP addresses for the services"
     echo "-n, --nginx: Set up nginx configuration"
     echo "-a, --all: Clean, build and run all services"
+    echo "\n"
 }
 
 if(( $# == 0 )); then
@@ -76,45 +96,50 @@ fi
 
 for i in "$@"
 do
-case $i in
-    -h|--help)
-    show_help
-    shift
-    ;;
-    -c|--clean)
-    clean
-    shift
-    ;;
-    -b|--backend)
-    run_backend
-    shift
-    ;;
-    -f|--front)
-    run_frontend
-    shift
-    ;;
-    -d|--docker-compose)
-    compose
-    shift
-    ;;
-    -C|--config-dns)
-    config_dns
-    shift
-    ;;
-    -n|--nginx)
-    set_nginx_config
-    shift
-    ;;
-    -a | --all)
-    clean
-    config_dns
-    run_backend
-    run_frontend
-    compose
-    set_nginx_config
-    shift
-    ;;
-    *)
-    ;;
-esac
+    case $i in
+        -h|--help)
+            show_help
+            shift
+        ;;
+        -c|--clean)
+            clean
+            shift
+        ;;
+        -b|--backend)
+            build_backend
+            shift
+        ;;
+        -f|--front)
+            build_frontend
+            shift
+        ;;
+        -p|--push)
+            push_images
+            shift
+        ;;
+        -d|--docker-compose)
+            compose
+            shift
+        ;;
+        -C|--config-dns)
+            config_dns
+            shift
+        ;;
+        -n|--nginx)
+            set_nginx_config
+            shift
+        ;;
+        -a | --all)
+            clean
+            build_backend
+            build_frontend
+            push_images
+            compose
+            config_dns
+            set_nginx_config
+            shift
+        ;;
+        *)
+        ;;
+    esac
 done
